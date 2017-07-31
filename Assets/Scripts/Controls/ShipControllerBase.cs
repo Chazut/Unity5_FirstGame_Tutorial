8157ef6fce06743debfcce907c8aa59d05b551b9
Chazut_MainPC <jagut.jeremy@gmail.com>
2017-08-01 00:18:35 +0200

    024 - Value Mapping - C2S3S8

--- Assets/Scripts/Controls/ShipControllerBase.cs
+++ Assets/Scripts/Controls/ShipControllerBase.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public abstract class ShipControllerBase : MonoBehaviour, IControllable
 {
+    public AnimationCurve steeringCurve;
+    public AnimationCurve glideCurve;
+
+    private float rearPowerLimit = 0.2f;
+    private Vector2 _thrust_tmp;
 
     private Vector2 _thrust;
     protected Vector2 Thrust
@@ -35,8 +40,15 @@ public abstract class ShipControllerBase : MonoBehaviour, IControllable
 
     public virtual void Move(Vector2 movement)
     {
-        Steering = movement.x;
-        Thrust = movement;
+        movement = Vector2.ClampMagnitude(movement, 1.0f);
+        movement.y = Mathf.Clamp(movement.y, -rearPowerLimit, 1);
+
+        //Steering = movement.x;
+        Steering = steeringCurve.Evaluate(movement.y) * movement.x;
+
+        _thrust_tmp.x = glideCurve.Evaluate(movement.y) * movement.x;
+        _thrust_tmp.y = movement.y;
+        Thrust = _thrust_tmp;
 
         enabled = (movement != Vector2.zero);
     }
