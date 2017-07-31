7a71f5162e8e00ad0243625aa2426c8d26cc4666
Chazut_MainPC <jagut.jeremy@gmail.com>
2017-07-31 18:52:00 +0200

    021 - Explosions 1ère partie - C2S2S5

---
+++ Assets/Scripts/AutoRelease.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AutoRelease : MonoBehaviour {
+
+    public float duration;
+
+    private void Awake()
+    {
+        Animator animator = GetComponent<Animator>();
+        float animatorLenght = animator ? animator.GetCurrentAnimatorClipInfo(0)[0].clip.length : 0;
+
+        AudioSource audio = GetComponent<AudioSource>();
+        float audioLenght = audio ? audio.clip.length : 0;
+
+        if(duration == 0)
+            duration = Mathf.Max(animatorLenght, audioLenght);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(Release());
+    }
+
+    private IEnumerator Release ()
+    {
+        yield return new WaitForSeconds(duration);
+        ObjectPool.Release(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayAudioOnEnable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class PlayAudioOnEnable : MonoBehaviour {
+
+    private AudioSource _audio;
+
+    private void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        _audio.Play();
+    }
+
+}
